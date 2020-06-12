cd8313d6d629e1520d391b55150de09b97d942c9
imtpj10 <tpj10@iitbbs.ac.in>
2020-06-13 01:27:18 +0530

    Changed to Field Injetion

--- src/main/java/com/demoGuice/request/ShapeRequest.java
+++ src/main/java/com/demoGuice/request/ShapeRequest.java
@@ -4,12 +4,14 @@ import com.demoGuice.services.DrawShape;
 import com.google.inject.Inject;
 
 public class ShapeRequest {
-     DrawShape drawShape;
 
     @Inject
-    public void setDrawShape(DrawShape drawShape) {
-        this.drawShape = drawShape;
-    }
+     DrawShape drawShape;
+
+
+//    public void setDrawShape(DrawShape drawShape) {
+//        this.drawShape = drawShape;
+//    }
     //public ShapeRequest(DrawShape d) {
     //    this.drawShape = d;
     //}
