a4f43422377ad46bb2958f70f81aaab4f3b626c5
imtpj10 <tpj10@iitbbs.ac.in>
2020-06-12 23:02:25 +0530

    binding to subclass

--- src/main/java/com/demoGuice/modules/AppModules.java
+++ src/main/java/com/demoGuice/modules/AppModules.java
@@ -1,13 +1,17 @@
 package com.demoGuice.modules;
 
+import com.demoGuice.request.ShapeRequest;
+import com.demoGuice.request.ShapeRequestSubClass;
 import com.demoGuice.services.DrawShape;
 import com.demoGuice.services.impl.DrawSquare;
 import com.google.inject.AbstractModule;
 
 
 public class AppModules extends AbstractModule {
+
     @Override
     protected void configure() {
         bind(DrawShape.class).to(DrawSquare.class);
+        bind(ShapeRequest.class).to(ShapeRequestSubClass.class);
     }
 }
---
+++ src/main/java/com/demoGuice/request/ShapeRequestSubClass.java
@@ -0,0 +1,18 @@
+package com.demoGuice.request;
+
+import com.demoGuice.services.DrawShape;
+import com.google.inject.Inject;
+
+public class ShapeRequestSubClass extends ShapeRequest {
+
+    @Inject
+    public ShapeRequestSubClass(DrawShape d) {
+        super(d);
+    }
+
+    @Override
+    public void makeRequest() {
+        System.out.println("This is subclass");
+        d.drawShape();
+    }
+}
